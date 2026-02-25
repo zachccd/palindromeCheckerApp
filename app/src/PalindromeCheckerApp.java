d30ea526ec7d6eea157c7854a989c39ea674b891
zachccd <uselessmail2406@gmail.com>
2026-02-25 14:49:54 +0530

    added started uc1

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerApp {
+    static void main(){
+        System.out.println("Welcome to PalindromeCheckerApp");
+        System.out.println("----");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
